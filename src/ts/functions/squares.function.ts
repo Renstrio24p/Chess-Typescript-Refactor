7996a720fec5572dba84dfbf01efa5c22ca0ad19
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-08-21 18:57:38 +0800

    update

--- src/ts/functions/squares.function.ts
+++ src/ts/functions/squares.function.ts
@@ -6,20 +6,21 @@ export function isSquareOccupied(square: HTMLDivElement | null) {
         if (pieces.length > 0) {
             const Color = pieces[0].getAttribute('color');
             return Color;
+        } else {
+            return 'blank';
         }
     }
-    return 'blank';
 }
 
-export function getAPieceAtSquare(squareID: HTMLDivElement | null, SquareBoardsArray: any) {
-   let SquareCurrent = SquareBoardsArray.find((element: any)=>{element.squareID===squareID}),
-       color = SquareCurrent.ColorPiece,
-       TypePiece = SquareCurrent.TypePiece,
-       PiecesID = SquareCurrent.PiecesID;
+export function getAPieceAtSquare(squareID: HTMLDivElement,SquareBoardsArray: any){
+    let SquareCurrent = SquareBoardsArray.find((element: any) => element.squareID === squareID)
+    const color = SquareCurrent.ColorPiece,
+          TypesPiece = SquareCurrent.TypesPiece,
+          PiecesID = SquareCurrent.PiecesID;
     
     return {
-        ColorPiece:color,
-        TypePiece:TypePiece,
-        PiecesID:PiecesID,
-    }
+        ColorPiece: color,
+        TypesPiece: TypesPiece,
+        PiecesID: PiecesID
+    };
 }
